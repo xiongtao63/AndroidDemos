807255bc2b80639835bebc9ef298a7aa984a9d23
xiongtao <xiongtao@sunline.com>
2022-10-31 10:27:34 +0800

    add java-lib java-project

---
+++ java_test_lib/src/main/java/com/xiongtao/java_test_lib/MyClass.java
@@ -0,0 +1,7 @@
+package com.xiongtao.java_test_lib;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
---
+++ java_test_project/src/Test.java
@@ -0,0 +1,7 @@
+//Tools->Internal Actions->Android->new Module->java->finish
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("java 项目"+args[0]);
+    }
+}
